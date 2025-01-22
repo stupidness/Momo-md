69dc178542c9ffd1d363002fea03d5deeb423539
Malvin King <malvink003@gmail.com>
2025-01-22 14:42:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
+
+/*
+Project Name : MALVIN XMD
+Creator      : Malvin King ( Mr Lord Malvin )
+Repo         : https//github.com/kingmalvn/MALVIN-XMD
+Support      : wa.me/263714757857
+*/
+
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -5,27 +14,87 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "Put Session Id",  // ADD Your Session Id 
-MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
-PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
-OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
-BOT_NAME: process.env.BOT_NAME || "MOMO-MD",
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!CUSTOM-SETTING-CONFIG=====================
-AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
-AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
-ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
-ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
-AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!API-CONFIG=================================
-///===============================================DONT-CHANGE-API=========================================
-GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!ALIVE-CONFIG================================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gdvaaw.jpg", //add alive img url
-ALIVE_MSG: process.env.ALIVE_MSG || "HI AM WHATSAPP BOT", //add alive msg
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
+
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌 status auto seen
+
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌 if you want auto reply on status 
+
+AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*Status viewed by momo md*",
+// set the auto reply massage on status reply  
+
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+
+BOT_NAME: process.env.BOT_NAME || "MOMO MD",
+// add bot namw here for menu
+
+STICKER_NAME: process.env.STICKER_NAME || "xᴍᴅ",
+// type sticker pack name 
+
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// 📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍,💘,💞,💕,❣️,💔,❤️‍🔥,💐,🌸,💮,🪷,🏵️,🌹,🌺,🌻,🌼,🌷,🎉,🪀,🧸,🎉,",
+// chose custom react emojis by yourself 
+
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2349019760022",
+// add your bot owner number
+
+OWNER_NAME: process.env.OWNER_NAME || "Beyond Official",
+// add bot owner name
+
+DESCRIPTION: process.env.DESCRIPTION || "*© sᴛᴀʏ ᴄᴏɴɴᴇᴄᴛᴇᴅ ғᴏʀ ғᴀɴᴛᴀsᴛɪᴄ ᴜᴘᴅᴀᴛᴇs!*",
+// add bot owner name 
+
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/f9k12w.jpg",
+// add img for alive msg
+
+LIVE_MSG: process.env.LIVE_MSG || "> ɴᴇᴠᴇʀ ᴀʟɪᴠᴇ ʙᴜᴛ ᴀᴡᴀᴋᴇ*🤠",
+// add alive msg here 
+
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+
+ANTI_BAD: process.env.ANTI_BAD || "false",
+//  1📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+READ_CMD: process.env.READ_CMD || "false",
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
+
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+//  📌 𝘁𝗿𝘂𝗲 𝗼𝗿 𝗳𝗮𝗹𝘀𝗲 𝗮𝘀 𝗬𝗼𝘂 𝗟𝗶𝗸𝗲 📌
 };
