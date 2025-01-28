c4353fcb4ebb1cedfbe4ac5951e1c52e1e7dd39f
stupidness <stupidtest453@gmail.com>
2025-01-28 22:14:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XMD~Z15jkRhL#qUCMmbx6ZLvb7nw3SlXfOkgbT-wkIK8anEb3nPoV1Q4",
 // add your Session Id 
 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
@@ -44,7 +44,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2349019760022",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2349063292134",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "Beyond Official",
